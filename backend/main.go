15e7b3967512ee3373d99495b6a6dfd78f5cca85
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2026-01-13 12:52:14 +0300

    feat: Initialize backend with Go module, main application entry point, and migration setup for database management

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+// main.go
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	_ "github.com/sonmezerekrem/linknlink/backend/migrations"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Dashboard
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
